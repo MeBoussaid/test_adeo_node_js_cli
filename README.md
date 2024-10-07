dbd0349ef4291684e53069ed90db037062bb4966
M. Boussaid <elie.boussaid@gmail.com>
2024-10-07 20:47:08 +0200

    one unit test

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This a test for ADEO, creating a CLI tool with Node.js
 
+npm install
+npm test
 node app.js --filter=yh
 node app.js --count
--- count.test.js
+++ count.test.js
@@ -1,7 +1,127 @@
 const { countByCountry, countByPerson } = require("./count.js");
-// normally we never use 'real' data in tests, we use some 'fixtures'
-// but here 'real' data is static and included in the repo, so let's use it in the tests too
-const { data } = require("./data/data.js");
 
-describe("countByPerson function tests", () => {});
+const sampleData = [
+  {
+    name: "Dillauti",
+    people: [
+      {
+        name: "Winifred Graham",
+        animals: [
+          { name: "Anoa" },
+          { name: "Duck" },
+          { name: "Narwhal" },
+          { name: "Badger" },
+          { name: "Cobra" },
+          { name: "Crow" },
+        ],
+      },
+      {
+        name: "Blanche Viciani",
+        animals: [
+          { name: "Barbet" },
+          { name: "Rhea" },
+          { name: "Snakes" },
+          { name: "Antelope" },
+          { name: "Echidna" },
+          { name: "Crow" },
+          { name: "Guinea Fowl" },
+          { name: "Deer Mouse" },
+        ],
+      },
+      {
+        name: "Philip Murray",
+        animals: [
+          { name: "Sand Dollar" },
+          { name: "Buzzard" },
+          { name: "Elephant" },
+          { name: "Xenops" },
+          { name: "Dormouse" },
+          { name: "Anchovy" },
+          { name: "Dinosaur" },
+        ],
+      },
+      {
+        name: "Bobby Ristori",
+        animals: [
+          { name: "Kowari" },
+          { name: "Caecilian" },
+          { name: "Common Genet" },
+          { name: "Chipmunk" },
+          { name: "Aardwolf" },
+          { name: "Przewalski's Horse" },
+          { name: "Badger" },
+          { name: "Sand Cat" },
+          { name: "Linne's Two-toed Sloth" },
+        ],
+      },
+      {
+        name: "Louise Pinzauti",
+        animals: [
+          { name: "Manta Ray" },
+          { name: "Nubian Ibex" },
+          { name: "Warbler" },
+          { name: "Duck" },
+          { name: "Mice" },
+        ],
+      },
+    ],
+  },
+];
+describe("countByPerson", () => {
+  it("Should add the number of animals to each person name", () => {
+    const somePeople = [
+      {
+        name: "Winifred Graham",
+        animals: [
+          { name: "Anoa" },
+          { name: "Duck" },
+          { name: "Narwhal" },
+          { name: "Badger" },
+          { name: "Cobra" },
+          { name: "Crow" },
+        ],
+      },
+      {
+        name: "Blanche Viciani",
+        animals: [
+          { name: "Barbet" },
+          { name: "Rhea" },
+          { name: "Snakes" },
+          { name: "Antelope" },
+          { name: "Echidna" },
+          { name: "Crow" },
+          { name: "Guinea Fowl" },
+          { name: "Deer Mouse" },
+        ],
+      },
+    ];
+    const result = countByPerson(somePeople);
+    expect(result).toEqual([
+      {
+        name: "Winifred Graham [6]",
+        animals: [
+          { name: "Anoa" },
+          { name: "Duck" },
+          { name: "Narwhal" },
+          { name: "Badger" },
+          { name: "Cobra" },
+          { name: "Crow" },
+        ],
+      },
+      {
+        name: "Blanche Viciani [8]",
+        animals: [
+          { name: "Barbet" },
+          { name: "Rhea" },
+          { name: "Snakes" },
+          { name: "Antelope" },
+          { name: "Echidna" },
+          { name: "Crow" },
+          { name: "Guinea Fowl" },
+          { name: "Deer Mouse" },
+        ],
+      },
+    ]);
+  });
+});
 // describe("countByCountry function tests", () => {});
