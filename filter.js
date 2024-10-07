3c8e53f468db0272305489ef6f2f653bca77ac14
MB <mehdi.boussaid.free@gmail.com>
2024-10-07 13:59:07 +0200

    first test - without CLI logic

---
+++ filter.js
@@ -0,0 +1,62 @@
+const { data } = require("./data.js");
+
+function filter(data, pattern) {
+  const trimmedPattern = pattern.trim();
+
+  if (!trimmedPattern) {
+    console.log("Invalid pattern: thepattern cannot be empty or white spaces.");
+    return null;
+  }
+
+  const filteredCountries = filterCountries(data, trimmedPattern);
+
+  if (filteredCountries.length === 0) {
+    console.log("No  animals found for the given pattern.");
+    return null;
+  }
+
+  return filteredCountries;
+}
+
+function filterCountries(countries, pattern) {
+  return countries
+    .map((country) => {
+      const filteredPeople = filterPeopleOfACountry(country.people, pattern);
+
+      if (filteredPeople.length > 0) {
+        return {
+          name: country.name,
+          people: filteredPeople,
+        };
+      }
+
+      return null;
+    })
+    .filter((country) => country !== null);
+}
+
+function filterPeopleOfACountry(people, pattern) {
+  return people
+    .map((person) => {
+      const filteredAnimals = filterAnimalsOfAPerson(person.animals, pattern);
+
+      if (filteredAnimals.length > 0) {
+        return {
+          name: person.name,
+          animals: filteredAnimals,
+        };
+      }
+
+      return null;
+    })
+    .filter((person) => person !== null);
+}
+
+function filterAnimalsOfAPerson(animals, pattern) {
+  return animals.filter((animal) => animal.name.includes(pattern));
+}
+
+const pattern = " ";
+// const filteredData = filter(data, pattern);
+
+// console.log("filteredData :", filteredData);
